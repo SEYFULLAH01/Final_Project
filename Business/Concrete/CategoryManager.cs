90407f914bc369f7a2a4adb437c93de6ac336336
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2023-11-02 23:14:57 +0300

    Test1

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public List<Category> GetById(int categoryId)
+        {
+            var category = _categoryDal.Get(c => c.CategoryId == categoryId);
+            List<Entities.Concrete.Category> categoryList = new List<Entities.Concrete.Category> { category };
+            return categoryList;
+        }
+    }
+}
