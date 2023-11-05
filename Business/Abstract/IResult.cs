b103dd5025084d678b43046c5bd5031c84dbaa00
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2023-11-06 00:14:28 +0300

    05.11.2023

--- Business/Abstract/IResult.cs
+++ Business/Abstract/IResult.cs
@@ -2,5 +2,6 @@
 {
     public interface IResult
     {
+        bool Success { get; }
     }
 }
---
+++ WebAPI/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Business.Absract;
+using Business.Concrete;
+using DataAccess.Absract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI
+{
+    public static class ServiceRegistration
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<IProductService, ProductManager>();
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+    }
+}
